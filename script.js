0b7f5bc427a3e9d26eadf81ca2537a44aeb3a3ab
Ubair Javid <enunmuz@users.noreply.github.com>
2020-01-31 20:47:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (!location.hash) {
 const roomHash = location.hash.substring(1);
 
 // TODO: Replace with your own channel ID
-const drone = new ScaleDrone("uNj04mqxLk247RTW");
+const drone = new ScaleDrone('uNj04mqxLk247RTW');
 // Room name needs to be prefixed with 'observable-'
 const roomName = "observable-" + roomHash;
 const configuration = {
